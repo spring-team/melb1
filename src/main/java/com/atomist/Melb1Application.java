1fa9a2d88bbf265217ebbaed811ca0ec8181a2fc
Rod Johnson <johnsonroda@gmail.com>
2018-04-30 21:44:32 +1000

    Everyone needs encouragement to write Java

--- src/main/java/com/atomist/Melb1Application.java
+++ src/main/java/com/atomist/Melb1Application.java
@@ -1,3 +1,5 @@
+// As I say yes to life, life says yes to me.
+
 // You're good enough, you're smart enough, and doggone it, people like you.
 
 // You are the delta in what you do, not the things you did in the past
