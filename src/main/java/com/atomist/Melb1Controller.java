256ae1276e690e517210292df761cd09f14b30fc
Rod Johnson <johnsonroda@gmail.com>
2018-05-01 18:24:21 +1000

    Everyone needs encouragement to write Java

--- src/main/java/com/atomist/Melb1Controller.java
+++ src/main/java/com/atomist/Melb1Controller.java
@@ -1,3 +1,5 @@
+// Deep at the center of my being is an infinite well of love.
+
 // You are the delta in what you do, not the things you did in the past
 
 // I believe in you. You can do the thing!
